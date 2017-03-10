c2684e8dee74d4552787ee850ac988fef3beaaef
Koki Ibukuro <koki.ibukuro@gmail.com>
2017-03-10 12:25:08 +0900

    Add required frameworks in OnPostprocessBuild

--- Assets/SocialWorker/Editor/SocialWorkerPostProcessBuild.cs
+++ Assets/SocialWorker/Editor/SocialWorkerPostProcessBuild.cs
@@ -12,24 +12,40 @@ public static class SocialWorkerPostProcessBuild
     {
         if (target != BuildTarget.iOS) { return; }
 
-        string path = Path.Combine(pathToBuiltProject, "Info.plist");
-        var plist = new PlistDocument();
-        plist.ReadFromFile(path);
+        // framework settings
+        {
+            string path = pathToBuiltProject + "/Unity-iPhone.xcodeproj/project.pbxproj";
+            var project = new PBXProject();
+            project.ReadFromFile(path);
+            string guid = project.TargetGuidByName("Unity-iPhone");
 
-        PlistElementArray schemes = plist.root["LSApplicationQueriesSchemes"] as PlistElementArray;
-        if (schemes == null)
-        {
-            schemes = plist.root.CreateArray("LSApplicationQueriesSchemes");
+			project.AddFrameworkToProject(guid, "Social.framework", false);
+			project.AddFrameworkToProject(guid, "MessageUI.framework", false);
+
+			project.WriteToFile(path);
         }
-        string[] scheveValues = schemes.values.Select(e => (e as PlistElementString).value).ToArray();
-        if (!scheveValues.Contains("line"))
+
+        // plist settings
         {
-            schemes.AddString("line");
+            string path = Path.Combine(pathToBuiltProject, "Info.plist");
+            var plist = new PlistDocument();
+            plist.ReadFromFile(path);
+
+            PlistElementArray schemes = plist.root["LSApplicationQueriesSchemes"] as PlistElementArray;
+            if (schemes == null)
+            {
+                schemes = plist.root.CreateArray("LSApplicationQueriesSchemes");
+            }
+            string[] scheveValues = schemes.values.Select(e => (e as PlistElementString).value).ToArray();
+            if (!scheveValues.Contains("line"))
+            {
+                schemes.AddString("line");
+            }
+            if (!scheveValues.Contains("instagram"))
+            {
+                schemes.AddString("instagram");
+            }
+            plist.WriteToFile(path);
         }
-        if (!scheveValues.Contains("instagram"))
-        {
-            schemes.AddString("instagram");
-        }
-        plist.WriteToFile(path);
     }
 }
